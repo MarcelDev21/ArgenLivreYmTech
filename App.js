dfd7e35c015954f4bf164a78dd2c172aaad9c524
MarcelDev <marceldev96@gmail.com>
2022-01-12 22:37:28 +0100

    add tabNavigator

--- App.js
+++ App.js
@@ -2,9 +2,25 @@ import React from 'react';
 import {View, Text, TextInput} from 'react-native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from './Component/Home'
 import Profile from './Component/Profile';
+import Vue1 from './MesBandes/Vue1';
+import Vue2 from './MesBandes/Vue2';
+
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+
+
+const MyTabs =() => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={Vue1} />
+      <Tab.Screen name="Settings" component={Vue2} />
+    </Tab.Navigator>
+  );
+}
 
 
 const MyStack = ({navigation}) => {
@@ -15,12 +31,14 @@ const MyStack = ({navigation}) => {
        component={Home} />
       <Stack.Screen
        name="Profile"
-       component={Profile} />
+       component={MyTabs} />
       {/* <Stack.Screen name="Notifications" component={Notifications} />
       <Stack.Screen name="Settings" component={Settings} /> */}
   </Stack.Navigator>
   )
 }
+
+
 
 const App = () => {
   return (
